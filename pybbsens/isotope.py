29899472ee8f1090658351b548a5407f0255033b
Justo Martin-Albo <jmalbos@users.noreply.github.com>
2014-08-04 16:32:40 +0200

    Removed dependence in module isotope.

--- pybbsens/isotope.py
+++ pybbsens/isotope.py
@@ -1,22 +1,11 @@
 """docstring for module isotope"""
 
 import units
-import constants
-
-import math
-import os.path as op
-
-import pandas as pd
-
-
-
-NMEDB = pd.read_csv(op.join(op.dirname(__file__), 'NMEDatabase.csv'), index_col=0)
-
 
 class Isotope(object):
     """docstring for Isotope"""
 
-    def __init__(self, symbol, Z, A, W, Qbb, G0nu, NMEs):
+    def __init__(self, symbol, Z, A, W, Qbb, G0nu):
         super(Isotope, self).__init__()
         self.symbol = symbol
         self.Z = Z  # atomic number
@@ -24,120 +13,90 @@ class Isotope(object):
         self.W = W  # isotopic mass
         self.Qbb = Qbb  # Q value of double beta decay
         self.G0nu = G0nu  # phase-space factor
-        self.NMEs = NMEs  # nuclear matrix elements
-        self.set_M0nu('IBM2')
-
-    def set_M0nu(self, name):
-        self.M0nu = self.NMEs[name] * (NMEDB.loc[name]['g_A'])**2
-
-    def specific_sensitivity(self):
-        """docstring"""
-        return math.sqrt(self.W * constants.m_e**2 / (constants.N_A \
-            * math.log(2.) * self.G0nu * self.M0nu**2))
 
 
+############################################################
+### DOUBLE BETA DECAY ISOTOPES
 
-##############################
-
-symbol = 'Ca48'
+### CALCIUM-48
 W    = 47.95 *units.gram/units.mole
 Qbb  = 4263. *units.keV
 G0nu = 24.81E-15 /units.year
-NMEs = NMEDB[symbol]
 
-Ca48 = Isotope(symbol, 20, 48, W, Qbb, G0nu, NMEs)
+Ca48 = Isotope('Ca48', 20, 48, W, Qbb, G0nu)
 
-##############################
-
-symbol = 'Ge76'
+### GERMANIUM-76
 W    = 75.92 *units.gram/units.mole
 Qbb  = 2039. *units.keV
 G0nu = 2.36E-15 /units.year
 
-Ge76 = Isotope(symbol, 32, 76, W, Qbb, G0nu, NMEDB[symbol])
+Ge76 = Isotope('Ge76', 32, 76, W, Qbb, G0nu)
 
-##############################
-
-symbol = 'Se82'
+### SELENIUM-82
 W    = 81.92 *units.gram/units.mole
 Qbb  = 2998. *units.keV
 G0nu = 10.16E-15 /units.year
 
-Se82 = Isotope(symbol, 34, 82, W, Qbb, G0nu, NMEDB[symbol])
+Se82 = Isotope('Se82', 34, 82, W, Qbb, G0nu)
 
-##############################
-
-symbol = 'Zr96'
+### ZIRCONIUM-96
 W    = 95.91 *units.gram/units.mole
 Qbb  = 3346. *units.keV
 G0nu = 20.58E-15 /units.year
 
-Zr96 = Isotope(symbol, 40, 96, W, Qbb, G0nu, NMEDB[symbol])
+Zr96 = Isotope('Zr96', 40, 96, W, Qbb, G0nu)
 
-##############################
-
-symbol = 'Mo100'
+### MOLYBDENUM-100
 W    = 99.91 *units.gram/units.mole
 Qbb  = 3034. *units.keV
 G0nu = 15.92E-15 /units.year
 
-Mo100 = Isotope(symbol, 42, 100, W, Qbb, G0nu, NMEDB[symbol])
+Mo100 = Isotope('Mo100', 42, 100, W, Qbb, G0nu)
 
-##############################
-
-symbol = 'Pd110'
+### PALLADIUM-110
 W    = 109.91 *units.gram/units.mole
 Qbb  = 2018. *units.keV
 G0nu = 4.82E-15 /units.year
 
-Pd110 = Isotope(symbol, 46, 110, W, Qbb, G0nu, NMEDB[symbol])
+Pd110 = Isotope('Pd110', 46, 110, W, Qbb, G0nu)
 
-##############################
-
-symbol = 'Cd116'
+### CADMIUM-116
 W    = 115.90 *units.gram/units.mole
 Qbb  = 2814. *units.keV
 G0nu = 16.70E-15 /units.year
 
-Cd116 = Isotope(symbol, 48, 116, W, Qbb, G0nu, NMEDB[symbol])
+Cd116 = Isotope('Cd116', 48, 116, W, Qbb, G0nu)
 
-##############################
-
-symbol = 'Sn124'
+### TIN-124
 W    = 123.91 *units.gram/units.mole
 Qbb  = 2287. *units.keV
 G0nu = 9.04E-15 /units.year
 
-Sn124 = Isotope(symbol, 50, 124, W, Qbb, G0nu, NMEDB[symbol])
+Sn124 = Isotope('Sn124', 50, 124, W, Qbb, G0nu)
 
-##############################
-
-symbol = 'Te130'
+### TELLURIUM-130
 W    = 129.91 *units.gram/units.mole
 Qbb  = 2528. *units.keV
 G0nu = 14.22E-15 /units.year
 
-Te130 = Isotope(symbol, 52, 130, W, Qbb, G0nu, NMEDB[symbol])
+Te130 = Isotope('Te-130', 52, 130, W, Qbb, G0nu)
 
-##############################
-
-symbol = 'Xe136'
+### XENON-136
 W    = 135.91 *units.gram/units.mole
 Qbb  = 2458. *units.keV
 G0nu = 14.58E-15 /units.year
 
-Xe136 = Isotope("Xe136", 54, 136, W, Qbb, G0nu, NMEDB[symbol])
+Xe136 = Isotope("Xe136", 54, 136, W, Qbb, G0nu)
 
-##############################
-
-symbol = 'Nd150'
+### NEODYMIUM-150
 W    = 149.92 *units.gram/units.mole
 Qbb  = 3371. *units.keV
 G0nu = 63.03E-15 /units.year
 
-Nd150 = Isotope(symbol, 60, 150, W, Qbb, G0nu, NMEDB[symbol])
+Nd150 = Isotope('Nd150', 60, 150, W, Qbb, G0nu)
 
-##############################
+
+### DICTIONARY
 
 isotopes = {Ca48.symbol: Ca48, Ge76.symbol: Ge76, Se82.symbol: Se82, 
             Zr96.symbol: Zr96, Mo100.symbol: Mo100, Pd110.symbol: Pd110, 
@@ -145,19 +104,6 @@ isotopes = {Ca48.symbol: Ca48, Ge76.symbol: Ge76, Se82.symbol: Se82,
             Xe136.symbol: Xe136, Nd150.symbol: Nd150}
 
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
 
-    order = {Ca48.symbol: 0, Ge76.symbol: 1, Se82.symbol: 2, 
-            Zr96.symbol: 3, Mo100.symbol: 4, Pd110.symbol: 5, 
-            Cd116.symbol: 6, Sn124.symbol: 7, Te130.symbol: 8, 
-            Xe136.symbol: 9, Nd150.symbol: 10}
-
-    for method in NMEDB.index:
-
-        print '## {} #####'.format(method)
-
-        for i in isotopes:
-            isotopes[i].set_M0nu(method)
-            print '{} = {}'.format(order[i], isotopes[i].specific_sensitivity() \
-                / (units.meV*math.sqrt(units.kg*units.year)) )
 
