997a6c787fe5d962bd92d1da347aba9757dfc9ea
Justo Martin-Albo <jmalbos@users.noreply.github.com>
2014-08-05 12:49:35 +0200

    Added a acript for the generation of lookup tables containing Feldman-Cousins average upper limits.

---
+++ examples/GenerateFCTables.py
@@ -0,0 +1,27 @@
+import sys
+sys.path.append('..')
+from pybbsens import conflimits
+
+DATA_PATH = '../data/'
+
+
+def GenerateFCTable(CL, bkg_min=0., bkg_max=100., step=1.0):
+    """Generate a table of Feldman-Cousins average upper limits 
+    between bkg_min and bkg_max for a given confidence level (CL)."""
+
+    filename = DATA_PATH + 'FC' + str(int(CL*100)) + '.dat'
+
+    print 'Generating table of Feldman-Cousins average upper values for CL=', CL
+    
+    fcm = conflimits.FCMemoizer(CL)
+    fcm.ComputeTableAverageUpperLimits(bkg_min, bkg_max, step, filename)
+
+
+if __name__ == '__main__':
+
+
+    
+    GenerateFCTable(0.68)
+    GenerateFCTable(0.90)
+    GenerateFCTable(0.95)
+    GenerateFCTable(0.99)
