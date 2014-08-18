65b9d33fbdb4e658f4dfa14d25188c043100eb7d
jjgomezcadenas <jjgomezcadenas@users.noreply.github.com>
2014-08-18 15:28:11 +0100

    Adding more examples

---
+++ examples/SuperNemo.py
@@ -0,0 +1,37 @@
+###
+
+import os.path
+FILE_PATH = os.path.dirname(os.path.realpath(__file__))
+DATA_PATH = FILE_PATH + '/../data/'
+import sys
+sys.path.append(FILE_PATH + '/..')
+
+from pybbsens import units
+from pybbsens import isotope
+from pybbsens import experiment
+from pybbsens import conflimits
+from pybbsens import nmeset
+
+
+name = "SN"
+isot = isotope.Se82
+eff  = 0.3
+eres = isot.Qbb * 0.04
+bkg  = 5.E-3 / (units.keV*units.kg*units.year)
+mass = 76. *units.kg
+expo = 100. *units.kg*units.year
+
+EXO200 = experiment.Experiment(name, isotope.Xe136, eff, eres, bkg, mass)
+
+FCM = conflimits.FCMemoizer(0.9)
+FCM.ReadTableAverageUpperLimits(DATA_PATH+'FC90.dat')
+
+nmes=["ISM","IBM2","QRPA_Tu","QRPA_Jy","EDF"]
+for nme in nmes:
+	print "NME = ", nme
+	isotope.SelectNMESet(nmeset.nmedb[nme])
+
+	mbb = EXO200.sensitivity(expo,FCM)
+	print "mbb (meV) =",mbb /units.meV
+	hl = isot.half_life(mbb)
+	print "Tonu (year) =",hl / units.year
