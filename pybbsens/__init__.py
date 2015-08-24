84e849b0c5954a550b10933d798f7c7db8e8660f
Justo Martin-Albo <jmalbos@users.noreply.github.com>
2015-08-24 10:56:11 +0200

    .

--- pybbsens/__init__.py
+++ pybbsens/__init__.py
@@ -1,4 +1,4 @@
 """Python library for the calculation of the sensitivity
-of a 0nubb-decay experiment."""
+of a double beta decay experiment."""
 
 __all__ = ["units", "constants", "nmeset", "isotope", "conflimits", "experiment"]
